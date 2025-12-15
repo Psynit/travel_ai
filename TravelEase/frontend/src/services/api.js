2912f29f53187d77fa7fb300879045abb22363e6
Aaliya <aaliyafathima444@gmail.com>
2025-12-16 00:23:39 +0530

    Update API URL to live backend

--- TravelEase/frontend/src/services/api.js
+++ TravelEase/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL:"https://travel-ai-1-5mzd.onrender.com",
     withCredentials: true, // Important for session cookies
     headers: {
         'Content-Type': 'application/json',
